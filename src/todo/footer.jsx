d5718e6d4e0b006876039b82080883378dc25a60
zhbfchina <zhbfchina@gmail.com>
2018-04-08 08:02:56 +0800

    add vueStudy

---
+++ src/todo/footer.jsx
@@ -0,0 +1,17 @@
+import '../assets/styles/footer.less'
+
+export default {
+    data() {
+        return {
+            author: 'Jokcy'
+        }
+
+    },
+    render() {
+        return (
+            <div id="footer">
+                <span>Written by {this.author}</span>
+            </div>
+        )
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,87 @@
+
+
+const webpack = require('webpack');
+const HTMLPlugin = require('html-webpack-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
+
+
+const config = {
+    target: 'web',
+    entry: __dirname + '/src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: __dirname + '/dist'
+    },
+    devtool: '#cheap-module-eval-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.jsx$/,
+                loader: 'babel-loader'
+            }
+            , {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }, {
+                test: /\.(gif|jpg|jpeg|svg|png)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 2048,
+                            name: '[name].[ext]'
+                        }
+                    }
+                ]
+            }, {
+                test: /\.less$/,
+                use: [
+
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader:'postcss-loader',
+                        options:{
+                            sourceMap:true,
+                        }
+                    },
+                    'less-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: isDev ? '"development"' : '"production"'
+            }
+        }),
+        new HTMLPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin()
+    ],
+    devServer: {
+        port: 8000,
+        host: '0.0.0.0',
+        overlay: {
+            error: true
+        },
+        hot: true
+        //historyFallback: {
+
+        //}
+        //open:true
+    }
+};
+
+module.exports = config;
+
+
